7eb754bfe5baafc0b30b09974ee801919ad355e2
viktoryboykova <viktoryboykova@list.ru>
2022-03-23 17:49:25 +0300

    0. RESTFull. Описание архитектуры [#6884]

--- src/main/java/ru/job4j/Job4jAuthApplication.java
+++ src/main/java/ru/job4j/Job4jAuthApplication.java
@@ -1,10 +1,28 @@
 package ru.job4j;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class Job4jAuthApplication {
+public class Job4jAuthApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Job4jAuthApplication.class);
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource ds) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+        liquibase.setDataSource(ds);
+        return liquibase;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Job4jAuthApplication.class, args);
---
+++ src/main/java/ru/job4j/domain/Person.java
@@ -0,0 +1,50 @@
+package ru.job4j.domain;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String login;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
